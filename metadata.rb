32e90e9d50f666f2c17ebc088ec53d530dd4e004
Lopaka Delp <lopaka@rightscale.com>
2015-06-19 13:28:22 -0700

    Added collectd script.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name        'sys'
 description 'System scripts'
 
-version     '1.0.0'
+version     '1.0.20150619'
 
 recipe      'sys::setup_hostname', 'Set hostname'
+recipe      'sys::setup_collectd', 'Set collectd'
 
 attribute   'HOSTNAME',
   :display_name => 'HOSTNAME',
@@ -12,3 +13,19 @@ attribute   'HOSTNAME',
   :type => 'string',
   :default => 'env:RS_SERVER_NAME',
   :recipes => ['sys::setup_hostname']
+
+# This is for collectd server
+attribute   "COLLECTD_SERVER",
+  :display_name => "RightScale monitoring server to send data to",
+  :required => "optional",
+  :type => "string",
+  :default => "env:RS_TSS",
+  :recipes => ["sys::setup_collectd"]
+
+attribute   "RS_INSTANCE_UUID",
+  :display_name => "RightScale monitoring ID for this server",
+  :required => "optional",
+  :type => "string",
+  :default => "env:RS_INSTANCE_UUID",
+  :recipes => ["sys::setup_collectd"]
+
